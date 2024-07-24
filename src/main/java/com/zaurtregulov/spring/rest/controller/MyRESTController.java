2a15273dd0d94456c7f35c9b905ca20655a613ac
Aida Sagadat <aidasagadat355@gmail.com>
2024-07-24 22:40:30 +0500

    Update code to catch some exceptions

--- src/main/java/com/zaurtregulov/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/zaurtregulov/spring/rest/controller/MyRESTController.java
@@ -1,8 +1,12 @@
 package com.zaurtregulov.spring.rest.controller;
 
 import com.zaurtregulov.spring.rest.entity.Employee;
+import com.zaurtregulov.spring.rest.exception_handling.ExceptionInformation;
+import com.zaurtregulov.spring.rest.exception_handling.NoSuchEmployeeException;
 import com.zaurtregulov.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,10 +30,32 @@ public class MyRESTController {
     public Employee getEmployee(@PathVariable int id){
         Employee employee = employeeService.getEmployee(id);
 
+        if(employee == null){
+            throw new NoSuchEmployeeException("Employee with id " + id + " not found");
+        }
+
         return employee;
 
     }
 
+    @ExceptionHandler
+    public ResponseEntity<ExceptionInformation> handleException(NoSuchEmployeeException exception){
+
+        ExceptionInformation exceptionInformation = new ExceptionInformation();
+
+        exceptionInformation.setInformation(exception.getMessage());
+
+        return new ResponseEntity<>(exceptionInformation, HttpStatus.NOT_FOUND);
+
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ExceptionInformation> handleException(Exception exception){
+        ExceptionInformation exceptionInformation = new ExceptionInformation();
+        exceptionInformation.setInformation(exception.getMessage());
+
+        return new ResponseEntity<>(exceptionInformation, HttpStatus.BAD_REQUEST);
+    }
 
 
 }
---
+++ src/main/java/com/zaurtregulov/spring/rest/exception_handling/ExceptionInformation.java
@@ -0,0 +1,18 @@
+package com.zaurtregulov.spring.rest.exception_handling;
+
+public class ExceptionInformation {
+
+    private String information;
+
+    public ExceptionInformation() {
+    }
+
+
+    public String getInformation() {
+        return information;
+    }
+
+    public void setInformation(String information) {
+        this.information = information;
+    }
+}
